d6a64238edfc90f3a6f30e18ffcdc2037d0a9378
the6p4c <me@doggirl.gay>
2016-03-04 19:26:58 +1000

    Angular functions and constants lists

--- index.js
+++ index.js
@@ -1,49 +1,69 @@
+var ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+
 $(document).ready(function() {
 	Graph.init("#graphContainer");
 	Graph.update([
-		{x: 1, y: 0},
-		{x: 2, y: 20},
-		{x: 3, y: 10},
-		{x: 4, y: 3},
-		{x: 5, y: -7},
-		{x: 6, y: 2},
-		{x: 7, y: -14},
-		{x: 8, y: 25},
-		{x: 9, y: 23},
-		{x: 10, y: -1}
+		{x: 1, y: 1},
+		{x: 2, y: 10},
+		{x: 3, y: 2},
+		{x: 4, y: 9},
+		{x: 5, y: 3},
+		{x: 6, y: 8},
+		{x: 7, y: 4},
+		{x: 8, y: 7},
+		{x: 9, y: 5},
+		{x: 10, y: 6}
 	]);
-
-	setTimeout(function() {
-		Graph.update([
-			{x: 1, y: 51},
-			{x: 2, y: 0.1},
-			{x: 3, y: -11},
-			{x: 4, y: 35},
-			{x: 5, y: 0.43532},
-			{x: 6, y: 12},
-			{x: 7, y: 0.2554132},
-			{x: 8, y: 10.4321},
-			{x: 9, y: 32},
-			{x: 10, y: -1.19},
-			{x: 11, y: 0.2554132},
-			{x: 12, y: 10.4321},
-			{x: 13, y: 22},
-			{x: 14, y: -1.19}
-		]);
-	}, 1000);
-
-	setTimeout(function() {
-		Graph.update([
-			{x: 1, y: 1},
-			{x: 2, y: 10},
-			{x: 3, y: 2},
-			{x: 4, y: 9},
-			{x: 5, y: 3},
-			{x: 6, y: 8},
-			{x: 7, y: 4},
-			{x: 8, y: 7},
-			{x: 9, y: 5},
-			{x: 10, y: 6}
-		]);
-	}, 2000);
 });
+
+angular.module("gseq", [])
+	.controller("InputController", function() {
+		var inputController = this;
+
+		inputController.functions = ["2", "3", "4"];
+		inputController.mainFunction = "f(n-1) / f(n-2) + f(n-3)";
+
+		inputController.getFunctions = function() {
+			var ret = {};
+
+			for (var i = 0; i < inputController.functions.length; ++i) {
+				ret[i] = inputController.functions[i];
+			}
+
+			ret['n'] = inputController.mainFunction;
+
+			return ret;
+		};
+
+		inputController.addFunction = function() {
+			inputController.functions.push("");
+		};
+
+		inputController.removeFunction = function(index) {
+			inputController.functions.splice(index, 1);
+		};
+
+		inputController.constants = ["10", "5", "2"];
+
+		inputController.getConstants = function() {
+			var ret = {};
+
+			for (var i = 0; i < inputController.constants.length; ++i) {
+				ret[ALPHABET[i]] = inputController.constants[i];
+			}
+
+			return ret;
+		};
+
+		inputController.addConstant = function() {
+			if (inputController.constants.length != ALPHABET.length) {
+				inputController.constants.push("");
+			} else {
+				alert("Cannot add new constant: constant limit reached");
+			}
+		};
+
+		inputController.removeConstant = function(name) {
+			inputController.constants.splice(ALPHABET.indexOf(name), 1);
+		};
+	});
